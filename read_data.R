e6b05f1757aaa7ceeb210606f0c734bdf6d9c51e
José C. Pernías <jcpernias@gmail.com>
2023-03-07 10:41:54 +0100

    Refactor code

--- read_data.R
+++ read_data.R
@@ -2,12 +2,12 @@ library(tidyverse)
 library(openxlsx)
 
 # IGAE data: Total government
-aapp_vars <-
-  tibble(row = c(7, 17, 36, 63, 64, 83),
-         vname = c("Gastos", "Intereses", "Ingresos",
-                   "SFiscal", "SPrimario", "PIB"))
 
 aapp <- local({
+  aapp_vars <-
+    tibble(row = c(7, 17, 36, 63, 64, 83),
+           vname = c("Gastos", "Intereses", "Ingresos",
+                     "SFiscal", "SPrimario", "PIB"))
   df_list <- list(
     read.xlsx("./data/A_AAPP.xlsx", "Tabla1a",
               colNames = TRUE,
@@ -17,41 +17,42 @@ aapp <- local({
               rows = c(6, aapp_vars$row[nrow(aapp_vars)])) |>
       mutate(CONCEPTOS  = as.character(CONCEPTOS)))
 
-  bind_rows(df_list)
-}) |>
-  select(-c("CÓDIGO", "CONCEPTOS")) |>
-  mutate(vnames = aapp_vars$vname) |>
-  rename("2021" = "2021(P)") |>
-  pivot_longer(cols = 1:27, names_to = "year") |>
-  mutate(year = as.integer(year)) |>
-  pivot_wider(names_from = vnames, values_from = value)
-
+  bind_rows(df_list)  |>
+    select(-c("CÓDIGO", "CONCEPTOS")) |>
+    mutate(vnames = aapp_vars$vname) |>
+    rename("2021" = "2021(P)") |>
+    pivot_longer(cols = 1:27, names_to = "year") |>
+    mutate(year = as.integer(year)) |>
+    pivot_wider(names_from = vnames, values_from = value)
+})
 
 # IGAE data: Taxes
-imp_vars <-
-  tibble(row = c(7, 74, 86, 95, 96, 97, 98),
-         vname = c("TInd", "TRenta", "TCap", "TTotal",
-                   "CotSS1", "CotSS2", "CotSS3"))
-imp <-
-    read.xlsx("./data/A_AAPP_Imp.xlsx", "Tabla1a",
-              colNames = TRUE,
-              rows = c(6, imp_vars$row)) |>
-  select(-c("CÓDIGO", "CONCEPTOS")) |>
-  mutate(vnames = imp_vars$vname) |>
-  rename("2021" = "2021(P)") |>
-  pivot_longer(cols = 1:27, names_to = "year") |>
-  mutate(year = as.integer(year)) |>
-  pivot_wider(names_from = vnames, values_from = value) |>
-  mutate(CotSS = CotSS1 + CotSS2 + CotSS3) |>
-  select(-(CotSS1:CotSS3))
+imp <- local({
+  imp_vars <-
+    tibble(row = c(7, 74, 86, 95, 96, 97, 98),
+           vname = c("TInd", "TRenta", "TCap", "TTotal",
+                     "CotSS1", "CotSS2", "CotSS3"))
+
+  read.xlsx("./data/A_AAPP_Imp.xlsx", "Tabla1a",
+            colNames = TRUE,
+            rows = c(6, imp_vars$row)) |>
+    select(-c("CÓDIGO", "CONCEPTOS")) |>
+    mutate(vnames = imp_vars$vname) |>
+    rename("2021" = "2021(P)") |>
+    pivot_longer(cols = 1:27, names_to = "year") |>
+    mutate(year = as.integer(year)) |>
+    pivot_wider(names_from = vnames, values_from = value) |>
+    mutate(CotSS = CotSS1 + CotSS2 + CotSS3) |>
+    select(-(CotSS1:CotSS3))
+})
 
 # IGAE data: COFOG
-cofog_vars <-
-  tibble(row = c(7, 15, 16, 22, 29, 39, 46, 53, 60, 67, 76),
-         vname = c("G01", "G01.8", "G02", "G03", "G04", "G05",
-                   "G06", "G07", "G08", "G09", "G10"))
-
 read_cofog <- function(level) {
+  cofog_vars <-
+    tibble(row = c(7, 15, 16, 22, 29, 39, 46, 53, 60, 67, 76),
+           vname = c("G01", "G01.8", "G02", "G03", "G04", "G05",
+                     "G06", "G07", "G08", "G09", "G10"))
+
   read.xlsx("./data/COFOG.xlsx", level,
             colNames = TRUE,
             rows = c(6, cofog_vars$row)) |>
